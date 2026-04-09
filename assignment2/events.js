85af9db3784db9ab5ec04ae812718bdf779a9db8
Emmanuel <ecobian12310@gmail.com>
2026-04-08 17:12:01 -0700

    done with task 1

---
+++ assignment2/events.js
@@ -0,0 +1,15 @@
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+module.exports = { emitter };
+
+emitter.on("time", (time) => {
+  console.log(`Time received: ${time}`);
+});
+
+emitter.on("error", (error) => {
+  console.log(`The emitter reported an error: ${error}`);
+});
+
+setInterval(() => {
+  emitter.emit("time", new Date().toString());
+}, 5000);
