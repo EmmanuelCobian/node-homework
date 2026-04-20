7b42dec2505a19e4ce4bf7cb835d87a51505996a
Emmanuel <ecobian12310@gmail.com>
2026-04-20 14:57:50 -0700

    added built-in middleware, custom middleware, and error handling

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -1,13 +1,66 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-const dogsRouter = require('./routes/dogs');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const dogsRouter = require("./routes/dogs");
 
 const app = express();
 
 // Your middleware here
+app.use((req, res, next) => {
+  req.requestId = uuidv4();
+  res.setHeader("X-Request-Id", req.requestId);
+  next();
+});
 
-app.use('/', dogsRouter); // Do not remove this line
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}]: ${req.method} ${req.path} (${req.requestId})`);
+  next();
+});
 
-const server =	app.listen(3000, () => console.log("Server listening on port 3000"));
+app.use((req, res, next) => {
+  if (req.method === "POST") {
+    const contentType = req.get("content-type") || "";
+    if (!contentType.includes("application/json")) {
+      const requestId = req.requestId;
+      return res.status(400).json({
+        error: "Content-Type must be application/json",
+        requestId: requestId,
+      });
+    }
+  }
+
+  next();
+});
+
+app.use(express.json({ limit: "1mb" }));
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use("/", dogsRouter); // Do not remove this line
+
+app.use((req, res, next) => {
+  if (!res.headersSent) {
+    return res
+      .status(404)
+      .json({ error: "Route not found", requestId: req.requestId });
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(
+    "Internal server error: ",
+    err.constructor.name,
+    JSON.stringify(err, ["name", "message"]),
+  );
+
+  res.status(500).json({
+    error: "Internal Server Error",
+    requestId: req.requestId,
+  });
+});
+
+const server = app.listen(3000, () =>
+  console.log("Server listening on port 3000"),
+);
 module.exports = server;
