363dcbe182da7223cceca0fe7c9a23ea90799b64
Emmanuel <ecobian12310@gmail.com>
2026-04-27 15:52:41 -0700

    storing only a hash of the passwords

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,23 @@
 const { StatusCodes } = require("http-status-codes");
 const { userSchema } = require("../validation/userSchema");
+const crypto = require("crypto");
+const util = require("util");
+const scrypt = util.promisify(crypto.scrypt);
 
-const register = (req, res) => {
+async function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const derivedKey = await scrypt(password, salt, 64);
+  return `${salt}:${derivedKey.toString("hex")}`;
+}
+
+async function comparePassword(inputPassword, storedHash) {
+  const [salt, key] = storedHash.split(":");
+  const keyBuffer = Buffer.from(key, "hex");
+  const derivedKey = await scrypt(inputPassword, salt, 64);
+  return crypto.timingSafeEqual(keyBuffer, derivedKey);
+}
+
+const register = async (req, res) => {
   if (!req.body) req.body = {};
   const { error, value } = userSchema.validate(
     { ...req.body },
@@ -11,16 +27,29 @@ const register = (req, res) => {
   if (error)
     return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
 
-  global.users.push(value);
-  global.user_id = value;
+  const newUser = {
+    name: value.name,
+    email: value.email,
+    password: await hashPassword(value.password),
+  };
+
+  global.users.push(newUser);
+  global.user_id = newUser;
   delete req.body.password;
   res.status(StatusCodes.CREATED).json(req.body);
 };
 
-const logon = (req, res) => {
+const logon = async (req, res) => {
   const { email, password } = { ...req.body };
   const user = global.users.find((user) => user.email === email);
-  if (user && user.password === password) {
+  if (!user) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Authentication Failed" });
+  }
+
+  const isMatch = await comparePassword(password, user.password);
+  if (isMatch) {
     global.user_id = user;
     res.status(StatusCodes.OK).json({ name: user.name, email: user.email });
   } else {
