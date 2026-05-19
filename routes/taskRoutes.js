ff29ec1f9879b46a67e57c39923320e5560cfdd2
Emmanuel <ecobian12310@gmail.com>
2026-05-19 16:32:16 -0700

    changed route to post rather than get

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -11,7 +11,7 @@ const {
 } = require("../controllers/taskController");
 
 router.route("/").get(index).post(create);
-router.route("/bulk").get(bulkCreate)
+router.route("/bulk").post(bulkCreate)
 router.route("/:id").get(show).patch(update).delete(deleteTask);
 
 module.exports = router;
